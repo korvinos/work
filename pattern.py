a7252584d210f9a149ae4d7bbad4bcb9703fb35d
korvinos <korvinos@bk.ru>
2016-09-02 19:18:10 +0300

    None

---
+++ pattern.py
@@ -0,0 +1,6 @@
+import custrepr
+
+path = '/home/artemm/Documents/Work/MichiganLake/TetsData/A2014301181500.L2_LAC_OC.x.nc'
+
+# custrepr.boreali_osw_processing(path, '/home/artemm/Documents/Work/MichiganLake/TetsData/Test/')
+custrepr.boreali_processing(path, '/home/artemm/Documents/Work/MichiganLake/TetsData/Test/')
