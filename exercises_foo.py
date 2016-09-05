15e4243919800f992ee1108aff82e789e4c1c699
korvinos <korvinos@bk.ru>
2016-09-05 17:11:25 +0300

    None

---
+++ exercises_foo.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as imread
+from nansat import Nansat, Domain, Mosaic
+from boreali import Boreali, lm
+import pandas as pd
+import csv
+
+
